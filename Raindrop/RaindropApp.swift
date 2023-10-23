f55b29496a664bcb189740df9c6e14f65a8aa3f9
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-23 14:28:35 +0300

    Initial Commit

---
+++ Raindrop/RaindropApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaindropApp.swift
+//  Raindrop
+//
+//  Created by Николай Игнатов on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RaindropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
