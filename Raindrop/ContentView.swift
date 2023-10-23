ce44c867e500367212968848f8b18db1e3461611
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-23 17:24:02 +0300

    add raindrop animation

--- Raindrop/ContentView.swift
+++ Raindrop/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragOffset: CGSize = .zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Canvas { context, size in
+            context.addFilter(.alphaThreshold(min: 0.5, color: .black))
+            context.addFilter(.blur(radius: 30))
+            
+            context.drawLayer { ctx in
+                for index in [1, 2] {
+                    if let resolvedView = context.resolveSymbol(id: index) {
+                        ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                    }
+                }
+            }
+        } symbols: {
+            Circle()
+                .fill(.black)
+                .frame(width: 150, height: 150)
+                .offset(.zero)
+                .tag(1)
+            
+            Circle()
+                .fill(.black)
+                .frame(width: 150, height: 150)
+                .offset(dragOffset)
+                .tag(2)
+            
         }
-        .padding()
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    dragOffset = value.translation
+                })
+                .onEnded({ _ in
+                    withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                        dragOffset = .zero
+                    }
+                })
+        )
     }
 }
 
